feacf134c70e6d2faeda4fef38dc373461b94b65
Karan Kumar <120077549+Karankumar80@users.noreply.github.com>
2024-07-22 12:50:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: 'sDocs',
-  description: 'Your go-to collaborative editor by IItian Karan',
+  description: 'Your go-to collaborative editor by IITian Karan',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
